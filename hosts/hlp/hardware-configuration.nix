507f0f906b8a915f42c0609e8fda5585a4b99fbf
xdHampus <xdhampus@null>
2022-05-10 11:12:12 +0200

    hlp

--- hosts/hlp/hardware-configuration.nix
+++ hosts/hlp/hardware-configuration.nix
@@ -8,23 +8,24 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4b8cd3e7-fa21-4f1e-ba4f-7781eb8a452c";
+    { device = "/dev/disk/by-uuid/b83f9419-be77-457c-a38a-1dfe2597b741";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/AE2F-8293";
+    { device = "/dev/disk/by-uuid/DC4E-8BC5";
       fsType = "vfat";
     };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/7c436b47-746d-47d4-b099-5e57a80b9a41"; }
+      { device = "/dev/disk/by-uuid/750618f3-a3f8-415e-a96b-3ddeb5e0e75d"; }
     ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
